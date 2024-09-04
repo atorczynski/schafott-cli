5c6a1d6fe1f20e2063b3c3b4a0e2eaed21d62b84
atorczynski <artur.torcz@gmail.com>
2024-09-04 19:13:44 +0200

    chore: Add .gitignore file to base files.

--- src/scaffolds/base/writeBaseFiles.ts
+++ src/scaffolds/base/writeBaseFiles.ts
@@ -3,12 +3,14 @@ import { writeFile } from '../../utils/writeHelpers';
 import { prettierConfig } from '../../scaffolds/globals/prettier.scaffold';
 import { lintConfig } from '../../scaffolds/globals/eslint.scaffold';
 import { exec } from 'child_process';
+import { gitignore } from '../globals';
 
 export const writeBaseFiles = async (options: Options) => {
   const { projectFeatures } = options;
 
   await writeFile('README.md', '# Project Title\n\nProject Description');
   await writeFile('eslint.config.js', lintConfig);
+  await writeFile('.gitignore', gitignore);
 
   if (projectFeatures.includes('prettier')) {
     await writeFile('.prettierrc', JSON.stringify(prettierConfig, null, 2));

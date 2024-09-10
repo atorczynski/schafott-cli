8953e5418692e7dfbe2541172a8beb6aba88c890
atoczynski <artur.torcz@gmail.com>
2024-09-11 01:51:21 +0200

    chore: Add scripts for publishing beta and next versions. NOTE Changesets solution seems far too abstract.

---
+++ .github/scripts/publish-version-beta.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import { exec } from 'child_process';
+
+// Read package.json
+const pkgJsonPath = '../../package.json';
+
+try {
+  const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
+  exec('git rev-parse --short HEAD', (error, stdout) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+    const commitHash = stdout.trim();
+    const betaVersion = `${pkgJson.version}-beta.${commitHash}`;
+    pkgJson.version = betaVersion;
+    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+    console.log(pkgJson.version);
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
